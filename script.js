2d0bb856c083e5bd2a2c87fa6b61d623b01cbf82
Frantisek Krus <krus.frantisek@gmail.com>
2023-02-06 17:15:32 +0100

    Navbar is mobile friendly and ready to use links not done

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const toggleButton = document.getElementById("nav-toggle");
-const nav = document.querySelector(".left-nav");
+const toggleBtn = document.querySelector('.nav-toggle');
+const leftNav = document.querySelector('.left-nav');
 
-toggleButton.addEventListener("click", function() {
-  nav.classList.toggle("show-nav");
+/* Make the navigation bar disappear when clicking elsewhere on the page */
+document.addEventListener('click', function(event) {
+  if (!event.target.closest('.left-nav') && !event.target.closest('.nav-toggle') && leftNav.classList.contains('show-nav')) {
+    leftNav.classList.remove('show-nav');
+    toggleBtn.style.display = 'block';
+  }
 });
+
+toggleBtn.addEventListener('click', function() {
+  leftNav.classList.toggle('show-nav');
+  
+  /* Make the toggle button disappear when the navigation bar is out */
+  if (leftNav.classList.contains('show-nav')) {
+    toggleBtn.style.display = 'none';
+  } else {
+    toggleBtn.style.display = 'block';
+  }
+});
+
